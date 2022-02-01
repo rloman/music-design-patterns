a89f6950fa4224d3f3313269fb5fbe6e39329666
Raymond Loman <raymond@carpago.nl>
2022-02-01 10:52:25 +0100

    Add lockObject for C#

--- src/nl/music/singleton/Dirigent.java
+++ src/nl/music/singleton/Dirigent.java
@@ -1,14 +1,29 @@
 package nl.music.singleton;
 
+import java.util.concurrent.locks.Lock;
+
 public class Dirigent {
 
     private static Dirigent instance;
+
+    private static Object lockObject = new Object();
 
     // let op dat je dit dus synchronized maakt. Of via lock met C#
     public static synchronized Dirigent getInstance() {
         if (instance == null) {
             instance = new Dirigent("Jaap van Zweden");
         }
+
+        return instance;
+    }
+
+    // c#
+    public static Dirigent getInstanceCSharp() {
+// uncomment this for C#       lock(lockObject) {
+            if (instance == null) {
+                instance = new Dirigent("Jaap van Zweden");
+            }
+// uncomment this for C#        }
 
         return instance;
     }
