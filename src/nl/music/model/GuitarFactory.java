e7c1a5bf97bd18d27031764f4ca8ed2696aa901e
Raymond Loman <raymond@carpago.nl>
2022-02-01 13:25:56 +0100

    Show Java's covariant return

--- src/nl/music/model/GuitarFactory.java
+++ src/nl/music/model/GuitarFactory.java
@@ -3,8 +3,7 @@ package nl.music.model;
 public class GuitarFactory extends InstrumentFactory {
 
     @Override
-    public Instrument getInstrument() {
-
+    public Guitar getInstrument() {
         return new Guitar("Brandless", 6); // moeilijker kan ik het niet maken, wel makkelijker
     }
 }
--- src/nl/music/model/PianoFactory.java
+++ src/nl/music/model/PianoFactory.java
@@ -3,7 +3,7 @@ package nl.music.model;
 public class PianoFactory extends InstrumentFactory {
 
     @Override
-    public Instrument getInstrument() {
+    public Piano getInstrument() {
         return new Piano("Guiness");
     }
 }
